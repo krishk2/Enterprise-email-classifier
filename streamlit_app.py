7ef611236d8a0f86fd4cd5b62ae7325c0e41d9e8
krishk2 <koushikpadigala2@gmail.com>
2026-01-19 13:36:01 +0530

    ..

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,14 @@ from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 import plotly.express as px
 
+hide_hd="""
+<style>
+header {visibility: hidden;}
+</style>
+"""
+st.markdown(hide_hd, unsafe_allow_html=True)
+
+
 # --- PAGE CONFIG ---
 st.set_page_config(
     page_title="Enterprise Email Classifier",
@@ -18,12 +26,6 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
-hide_hd="""
-<style>
-header {visibility: hidden;}
-</style>
-"""
-st.markdown(hide_hd, unsafe_allow_html=True)
 
 
 # --- PARTICLE BACKGROUND ---
